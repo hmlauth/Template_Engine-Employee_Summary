8486a5f7be8fe2417bb17c3c2a3618f419ab0bef
Andromeda <hannalauth@gmail.com>
2019-10-16 16:20:08 -0700

    revised classes so that parent class has getRole functionality and defaults to Employee if not provided with a title from child classes. Also refactored logic with async await functions. createTeam function returns an array of team members which can now be exported or used in any capacity

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Dependencies
+// // Dependencies
 const inquirer = require('inquirer');
 
 // Classes
@@ -8,69 +8,155 @@ const Intern = require('./lib/Intern');
 
 // Questions
 const {
-    menuQuestions, 
-    employeeQuestions, 
-    managerQuestions, 
-    engineerQuestions, 
+    menuQuestions,
+    employeeQuestions,
+    managerQuestions,
+    engineerQuestions,
     internQuestions
 } = require('./lib/questions');
 
-// HTML
-const displayHtml = require('./utils/displayHtml');
 
-// Employees
-const employeeList = [];
-
-// Functions
-function init() {
-    console.log('\nWelcome! Time to build your team:\n')
-    inquirer
-    .prompt(employeeQuestions.concat(managerQuestions))
-    .then(({name, id, email, officeNumber}) => {
-        let manager = new Manager(name, id, email, officeNumber);
-        employeeList.push(manager);
-        getEmployee();
-    })
+// Asks questions and returns Manager {name, id, email, officeNumber}
+async function getManager() {
+    try {
+        const { name, id, email, officeNumber } = await inquirer.prompt(employeeQuestions.concat(managerQuestions));
+        return new Manager(name, id, email, officeNumber)
+    } catch (err) {
+        throw err
+    }
 }
 
-function getEmployee() {
-    inquirer
-    .prompt(menuQuestions)
-    .then(({userChoice}) => {
-        switch (userChoice) {
-            case 'Engineer':
-                getEngineer();
-                break;
-            case 'Intern':
-                getIntern();
-                break;
-            case 'Exit':
-                displayHtml(employeeList);
-                // TODO - How to export employee list keeping functions and display logic separate
-                break;
+// Asks questions and returns Engineer {name, id, email, github}
+async function getEngineer() {
+    try {
+        const { name, id, email, githubUsername } = await inquirer.prompt(employeeQuestions.concat(engineerQuestions));
+        return new Engineer(name, id, email, githubUsername)
+    } catch (err) {
+        throw err
+    }
+}
+
+// Asks questions and returns Intern {name, id, email, school}
+async function getIntern() {
+    try {
+        const { name, id, email, school } = await inquirer.prompt(employeeQuestions.concat(internQuestions));
+        return new Intern(name, id, email, school)
+    } catch (err) {
+        throw err
+    }
+}
+
+// Returns an array of all team members
+async function createTeam() {
+    
+    const team = [];
+
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            // Get manager and push to team array
+            let manager = await getManager();
+            team.push(manager);
+    
+            // Get next employee
+            await getEmployee();
+
+            async function getEmployee() {
+                try {
+                inquirer
+                    .prompt(menuQuestions)
+                    .then(async ({ employeeType }) => {
+                        if (employeeType === "Engineer") {
+                            return { engineer } = await getEngineer();
+                        } else if (employeeType === "Intern") {
+                            return { intern } = await getIntern();
+                        } else {
+                            employee = null
+                        }
+                    }).then(employee => {
+                        if (employee) {
+                            team.push(employee);
+                            getEmployee();
+                        } else {
+                            resolve(team)
+                        }
+                    })
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+            
+    
+        } catch (err) {
+            console.log(err)
         }
     })
 }
 
-function getEngineer() {
-    inquirer
-    .prompt(employeeQuestions.concat(engineerQuestions))
-    .then(({name, id, email, githubUsername}) => {
-        let engineer = new Engineer(name, id, email, githubUsername);
-        employeeList.push(engineer);
-        getEmployee();
-    })
-}
 
-function getIntern() {
-    inquirer
-    .prompt(employeeQuestions.concat(internQuestions))
-    .then(({name, id, email, school})=> {
-        let intern = new Intern(name, id, email, school);
-        employeeList.push(intern)
-        getEmployee();
-    })
-}
+let finalTeam = await createTeam();
 
-// Methods
-init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var promise1 = new Promise(function(resolve, reject) {
+//     resolve('foo');
+// });
+
+// promise1.then(function(value) {
+//     console.log(value);
+//     // expected output: "foo"
+// });
+
+// console.log(promise1);
+// // expected output: [object Promise]    
+
+// getManager();
+// async function getManager() {
+
+//     inquirer
+//         .prompt(employeeQuestions.concat(managerQuestions))
+//         .then(manager => {
+//             manager = instantiateEmployeeType(manager);
+//             team.push(manager);
+//             getEmployee();
+//             function getEmployee() {
+//                 inquirer
+//                 .prompt(menuQuestions)
+//                 .then(({employeeType}) => {
+//                     if (employeeType === 'Engineer') {
+//                         return getEngineer()
+//                     } else if (employeeType === 'Intern') {
+//                         return getIntern()
+//                     } else {
+//                         return null
+//                     }
+//                 }).then(employee => {
+//                     if (employee) {
+//                         let teamMember = instantiateEmployeeType(employee)
+//                         team.push(teamMember);
+//                         getEmployee();
+//                     } else {
+//                         return;
+//                     }
+//                 })
+//             }
+//         })
+//         .catch(err => console.log(err))
+
+//         return new Promise((resolve, reject) => resolve(team));
+
+// }
